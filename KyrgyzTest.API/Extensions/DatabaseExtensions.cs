2b633b8df9bcc73db03fd812770f299d3e079797
Eraliev006 <eraliev.adilet006@gmail.com>
2026-03-30 23:20:02 +0300

    continue development

---
+++ KyrgyzTest.API/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using KyrgyzTest.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace KyrgyzTest.API.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static IServiceCollection AddDatabaseConnection(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        return services;
+    }
+}
---
+++ KyrgyzTest.API/Extensions/RepositoryExtension.cs
@@ -0,0 +1,17 @@
+﻿using KyrgyzTest.Core.Interfaces;
+using KyrgyzTest.Infrastructure.Repositories;
+
+namespace KyrgyzTest.API.Extensions;
+
+public static class RepositoryExtension
+{
+    public static IServiceCollection AddRepositories(
+        this IServiceCollection services)
+    {
+        services.AddScoped<ICandidateRepository, CandidateRepository>();
+        services.AddScoped<IExamSessionRepository, ExamSessionRepository>();
+        services.AddScoped<IComputerRepository, ComputerRepository>();
+
+        return services;
+    } 
+}
