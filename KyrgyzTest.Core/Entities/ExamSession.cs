cb25616acba9d29808b947f2523363c69c3f2a81
Eraliev006 <eraliev.adilet006@gmail.com>
2026-04-15 00:47:14 +0600

    feat(ExamSession): add ComputerId field

--- KyrgyzTest.Core/Entities/ExamSession.cs
+++ KyrgyzTest.Core/Entities/ExamSession.cs
@@ -4,7 +4,7 @@ public class ExamSession
 {
     public Guid Id { get; set; }
     public string ExamCode { get; set; }
-    public int StationNumber { get; set; }
+    public Guid ComputerId { get; set; }
     public DateTime StartAt { get; set; }
     public bool IsCompleted { get; set; }
 }
