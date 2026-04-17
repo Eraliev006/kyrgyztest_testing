f6e94711d86bae53bf78723d5c6f73b7957fa863
Eraliev006 <eraliev.adilet006@gmail.com>
2026-04-17 22:33:42 +0600

    add new migration

---
+++ KyrgyzTest.Infrastructure/Migrations/20260417163113_SeedSuperAdmin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KyrgyzTest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSuperAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "FullName", "Login", "PasswordHash", "Role" },
+                values: new object[] { new Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890"), new DateTime(2026, 4, 17, 16, 31, 13, 372, DateTimeKind.Utc).AddTicks(9540), "Super Admin", "superadmin", "$2a$11$YEhXcE2nILNSSsckAzsWo.33K5LhPC4vPU0uiDdgYin04.BaYo0BO", 0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Login",
+                table: "Users");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890"));
+        }
+    }
+}
